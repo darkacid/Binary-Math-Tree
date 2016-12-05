6c490267f389cc82cffa278c16a632776fdfd68a
Sergey Stepanyan <sergstep314@gmail.com>
2016-12-06 02:15:58 +0400

    Added the compute core
    Fly Safe

---
+++ src/Math/Calculate.java
@@ -0,0 +1,112 @@
+package Math;
+
+public class Calculate
+{
+
+	public static double compute(String commands)
+	{
+		//
+		//!The algorithm for generating the tree will be added later.!
+		//	
+		
+		
+		//The tree is made by hand		
+		node root=new node('+');
+		root.left=new node('*');
+		root.left.left=new node(7);
+		root.left.right=new node(2);
+		root.right=new node('/');
+		root.right.left=new node(25);
+		root.right.right=new node(5);
+		
+		System.out.println(exploreNode(root));
+		
+		return 0;
+	}
+	public static int exploreNode(node rootNode) //See the contents of the node.
+	{
+		
+		if(rootNode.isOp)
+		{
+			// The values for the numbers are gathered recursively.
+			int firstNum=exploreNode(rootNode.left);
+			int secondNum=exploreNode(rootNode.right);
+			
+			switch(rootNode.op)
+			{
+				case '+': return firstNum+secondNum;
+				case '-': return firstNum-secondNum;
+				case '*': return firstNum*secondNum;
+				case '/': return firstNum/secondNum;
+			}
+			
+		}
+		else
+			if(rootNode.number!=0)			
+				return rootNode.number;
+		
+		return -1;
+		
+	}
+	
+}
+class node
+{
+	//Each "node" gets either a number or an operator(+,-,* or /).
+	//The node will then contain an empty link to its left, and right "children" (it's called that way, I swear...).	
+	
+	int number; //This is zero by default, don't get confused.
+	char op;	//No clue what the default value is, too lazy to check.
+	boolean isOp=false;
+	node left;
+	node right;
+	
+	node(int number)
+	{
+		this.number=number;		
+	}
+	node(char op)
+	{
+		if(op=='+'||op=='-'||op=='*'||op=='/')
+		{
+			this.op=op;
+			this.isOp=true;			
+		}
+		else
+			System.out.println("Wrong Char");			
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//haha anarchy
---
+++ src/Math/KickStart.java
@@ -0,0 +1,18 @@
+package Math;
+
+public class KickStart {
+	
+	public static void main(String[] args)
+	{
+		//Calculate is a Class with the compute function, which is called without an instance.
+		
+		//The argument doesn't do anything yet.
+		//It will later accept some expression like "5*(1+2)".
+		Calculate.compute("1");
+		
+	}
+
+}
+
+
+//I solemnly swear to always start the comments with a capital letter and end it with a comma.
